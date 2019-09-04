a26c63fa3686b8918ad5e1811c58638906888c65
olshec <olshec@gmail.com>
2019-09-04 08:21:12 -0400

    finish email

--- action.php
+++ action.php
@@ -3,11 +3,14 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require './vendor/autoload.php';
+
+
 // require './vendor/phpmailer/phpmailer/src/Exception.php';
 // require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
 // require './vendor/phpmailer/phpmailer/src/SMTP.php';
 // Load Composer's autoloader
-require './vendor/autoload.php';
+
 
 session_start();
 
@@ -18,6 +21,8 @@ session_start();
 
 
 //myData
+$file_tmp=$_FILES["file1"]["tmp_name"];
+$file_name=$_FILES["file1"]["name"];
 $from = "diplom.website@yandex.ru";
 $to = $_POST['email'];
 $subject = $_POST['typeSelect'];
@@ -28,33 +33,17 @@ $headers = "From:" . $from;
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-    $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     // SMTP username
-    $mail->Password   = 'secret';                               // SMTP password
-    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
-    
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-    
+
+    $mail->setFrom('diplom.website@yandex.ru', 'Mailer');
+    $mail->addAddress($to, 'Joe User');    
     // Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    $mail->addAttachment($file_tmp, $file_name);         // Add attachments
     
     // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    //$mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     
     $mail->send();
     echo 'Message has been sent';
