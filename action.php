42b3e75a66e1fcdcdca2da9c10428d09e2b4b1e1
olshec <olshec@gmail.com>
2019-09-04 08:00:14 -0400

    add PHPMailer

--- action.php
+++ action.php
@@ -15,11 +15,6 @@ session_start();
 //move_uploaded_file($_FILES["file1"]["tmp_name"], $target_file);
 
 
-//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-//$namef=$_FILES["file1"]["tmp_name"];
-//$path =  realpath($namef);
-//echo sys_get_temp_dir()."<br>";
-//echo $path."<br>";
 
 
 //myData
@@ -29,62 +24,44 @@ $subject = $_POST['typeSelect'];
 $message = $_POST['message'];
 $headers = "From:" . $from;
 
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-//Get uploaded file data using $_FILES array
-$tmp_name    = $_FILES['file1']['tmp_name']; // get the temporary file name of the file on the server
-$file_name        = $_FILES['file1']['name'];  // get the name of the file
-$size        = $_FILES['file1']['size'];  // get size of the file for size validation
-$file_type        = $_FILES['file1']['type'];  // get type of the file
-$file_error       = $_FILES['file1']['error']; // get the error (if any)
-
-//validate form field for attaching the file
-if($file_error > 0)
-    {
-        die('Upload error or No files uploaded');
-    }
-
-// read from the uploaded file & base64_encode content
-$handle = fopen($tmp_name, "r"); // set the file handle only for reading the file
-$content = fread($handle, $size); // reading the file
-fclose($handle); // close upon completion
-
-$encoded_content = chunk_split(base64_encode($content));
-
-$boundary = md5("random"); // define boundary with a md5 hashed value
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     // SMTP username
+    $mail->Password   = 'secret';                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
     
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+    
+    // Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
-
-
-//header
-$headers = "MIME-Version: 1.0\r\n"; // Defining the MIME version
-$headers .= "From:".$from."\r\n"; // Sender Email
-//$headers .= "Reply-To: ".$reply_to_email."\r\n"; // Email addrress to reach back
-$headers .= "Content-Type: multipart/mixed;\r\n"; // Defining Content-Type
-$headers .= "boundary = $boundary\r\n"; //Defining the Boundary
-
-//plain text
-$body = "--$boundary\r\n";
-$body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
-$body .= "Content-Transfer-Encoding: base64\r\n\r\n";
-$body .= chunk_split(base64_encode($message));  
-
-//attachment
-$body .= "--$boundary\r\n";
-$body .="Content-Type: $file_type; name=".$tmp_name."\r\n";
-$body .="Content-Disposition: attachment; filename=".$tmp_name."\r\n";
-$body .="Content-Transfer-Encoding: base64\r\n";
-$body .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";
-$body .= $encoded_content; // Attaching the encoded file with
-
-//$res = mail($to,$subject,$message, $headers);
-$res = mail($to,$subject,$body, $headers);
-
-if($res)
-    echo 'Сообщение успешно отправлено!'."<br>";
-else
-    echo 'Нам не удалось отправить сообщение. Попытайтесь позже.'."<br>";
-        
-        
 
 if($_SESSION["captcha_code"] == $_POST["captcha_code"]){
     $_SESSION["captcha_code"]=null;
