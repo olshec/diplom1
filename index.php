1cb045fbbf63ee4ab327122c7aeafe4c86144eb3
olshec <olshec@gmail.com>
2019-09-05 07:16:39 -0400

    fix

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<?php
 
-
-use Gregwar\Captcha\CaptchaBuilder;
-
-require './vendor/autoload.php';
-
-session_start();
-
-$captcha = new CaptchaBuilder;
-$captcha->build();
-$_SESSION['captcha_code'] = $captcha->getPhrase();
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,6 +33,21 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script type="text/javascript" src="js/bootstrap-filestyle.min.js"> </script>
 </head>
+
+<?php
+
+
+use Gregwar\Captcha\CaptchaBuilder;
+
+require './vendor/autoload.php';
+
+session_start();
+
+$captcha = new CaptchaBuilder;
+$captcha->build();
+$_SESSION['captcha_code'] = $captcha->getPhrase();
+
+?>
 
 <body data-spy="scroll" data-target="#pb-navbar" data-offset="200">
 
