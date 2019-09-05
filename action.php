eed6315c25646881f78ca0f6847f0304432c34ef
olshec <olshec@gmail.com>
2019-09-05 07:12:48 -0400

    fix

--- action.php
+++ action.php
@@ -8,36 +8,37 @@ require './vendor/autoload.php';
 session_start();
 
 
+//myData
+$file_tmp=$_FILES["file1"]["tmp_name"];
+$file_name=$_FILES["file1"]["name"];
+$from_email = "diplom.website@yandex.ru";
+$to = $_POST['email'];
+$subject = $_POST['typeSelect'];
+$message = $_POST['message'];
+
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    
+    $mail->setFrom($from_email, 'Diplom');
+    $mail->addAddress($to, 'John');
+    $mail->addAttachment($file_tmp, $file_name);
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+    $mail->send();
+    echo 'Сообщение успешно отправлено';
+} catch (Exception $e) {
+    echo "Не удалось отправить сообщение. Ошибка: {$mail->ErrorInfo}";
+}
 
 
 if($_SESSION["captcha_code"] == $_POST["captcha_code"]){
     $_SESSION["captcha_code"]=null;
 
     
-    //myData
-    $file_tmp=$_FILES["file1"]["tmp_name"];
-    $file_name=$_FILES["file1"]["name"];
-    $from_email = "diplom.website@yandex.ru";
-    $to = $_POST['email'];
-    $subject = $_POST['typeSelect'];
-    $message = $_POST['message'];
-    
-    
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-    
-    try {
-        
-        $mail->setFrom($from_email, 'Diplom');
-        $mail->addAddress($to);
-        $mail->addAttachment($file_tmp, $file_name);
-        $mail->Subject = $subject;
-        $mail->Body    = $message;
-        $mail->send();
-        echo 'Сообщение успешно отправлено';
-    } catch (Exception $e) {
-        echo "Не удалось отправить сообщение. Ошибка: {$mail->ErrorInfo}";
-    }
+ 
     
 }
 else
