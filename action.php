009f5dd316203774a1a2966d4a1f0be7eba82215
olshec <olshec@gmail.com>
2019-09-12 05:47:33 -0400

    change action.php

--- action.php
+++ action.php
@@ -18,11 +18,12 @@ if($_SESSION["captcha_code"] == $_POST["captcha_code"]){
     $_SESSION["captcha_code"]=null;
     
     //myData
-    $file_tmp=$_FILES["file1"]["tmp_name"];
-    $file_name=$_FILES["file1"]["name"];
+    //$file_tmp=$_FILES["file1"]["tmp_name"];
+    //$file_name=$_FILES["file1"]["name"];
     $from_email = "diplom.website@yandex.ru";
-    $to = $_POST['email'];
-    $subject = $_POST['typeSelect'];
+    $to_email = $_POST['email'];
+    //$subject = $_POST['typeSelect'];
+    $work_theme = $_POST['work_theme'];
     $message = $_POST['message'];
     
     
@@ -31,13 +32,27 @@ if($_SESSION["captcha_code"] == $_POST["captcha_code"]){
     $mail->CharSet = 'UTF-8';
     try {
         
+        
+        
+        
+        
         $mail->setFrom($from_email, 'Diplom');
-        $mail->addAddress($to);
-        $mail->addAttachment($file_tmp, $file_name);
-        $mail->Subject = $subject;
+        $mail->addAddress($to_email);
+       // $mail->addAttachment($file_tmp, $file_name);
+        $mail->Subject = 'Ваш диплом';
+        $mail->Body    = 'Здравствуйте! Ваш заказ принят! Мы ответим вам в течение суток.';
+        $mail->send();
+        
+        $mail = new PHPMailer(true);
+        $mail->setFrom('new_zakaz@yandex.ru', 'Diplom');
+        $mail->addAddress($from_email);
+        $mail->Subject = $work_theme;
         $mail->Body    = $message;
         $mail->send();
-        echo 'Сообщение успешно отправлено';
+        
+        echo 'Ваш заказ принят! Мы ответим вам в течение суток.';
+ 
+        
     } catch (Exception $e) {
         echo "Не удалось отправить сообщение. Ошибка: {$mail->ErrorInfo}";
     }
