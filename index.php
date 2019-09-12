567cc68acf39a405e4c5a6d7b132dc286c9d5a82
olshec <olshec@gmail.com>
2019-09-12 17:19:05 -0400

    change template

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
   </section>
   <!-- END section -->
 
-  <section class="pb_section bg-light pb_slant-white pb_pb-250" id="section-features">
+  <section class="pb_section bg-light pb_slant-white pb_pb-220" id="section-features">
     <div class="container">
       <div class="row justify-content-center mb-5">
         <div class="col-md-6 text-center mb-5">
@@ -266,77 +266,8 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
   
   
 
-  <section class="pb_section bg-light pb_slant-white">
-    <div class="container">
-      <div class="row">
-<!--         <div class="col-lg-5 order-2"> -->
-<!--           <img src="assets/images/phone_3.png" alt="Image placeholder" class="img-fluid"> -->
-<!--         </div> -->
-        <div class="col-lg-12 pr-md-5 pr-sm-0 order-1  mb-5">
-          <div class="row">
-            <div class="col">
-              <h2>Application Features</h2>
-              <p class="pb_font-20">Far far away, behind the word mountains, far from the countries Vokalia and
-                Consonantia.</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg">
 
-              <div class="media pb_feature-v2 text-left mb-1 mt-5">
-                <div class="pb_icon d-flex mr-3 align-self-start pb_w-15"><i
-                    class="ion-ios-bookmarks-outline pb_icon-gradient"></i></div>
-                <div class="media-body">
-                  <h3 class="mt-2 mb-2 heading">Minimal Design</h3>
-                  <p class="text-sans-serif pb_font-16">Far far away, behind the word mountains, far from the countries
-                    Vokalia and Consonantia.</p>
-                </div>
-              </div>
-
-              <div class="media pb_feature-v2 text-left mb-1 mt-5">
-                <div class="pb_icon d-flex mr-3 align-self-start pb_w-15"><i
-                    class="ion-ios-infinite-outline pb_icon-gradient"></i></div>
-                <div class="media-body">
-                  <h3 class="mt-2 mb-2 heading">Unlimited Posibilities</h3>
-                  <p class="text-sans-serif pb_font-16">Far far away, behind the word mountains, far from the countries
-                    Vokalia and Consonantia.</p>
-                </div>
-              </div>
-
-            </div>
-            <div class="col-lg">
-
-              <div class="media pb_feature-v2 text-left mb-1 mt-5">
-                <div class="pb_icon d-flex mr-3 align-self-start pb_w-15"><i
-                    class="ion-ios-speedometer-outline pb_icon-gradient"></i></div>
-                <div class="media-body">
-                  <h3 class="mt-2 mb-2 heading">Fast Loading</h3>
-                  <p class="text-sans-serif pb_font-16">Far far away, behind the word mountains, far from the countries
-                    Vokalia and Consonantia.</p>
-                </div>
-              </div>
-
-              <div class="media pb_feature-v2 text-left mb-1 mt-5">
-                <div class="pb_icon d-flex mr-3 align-self-start pb_w-15"><i
-                    class="ion-ios-color-filter-outline  pb_icon-gradient"></i></div>
-                <div class="media-body">
-                  <h3 class="mt-2 mb-2 heading">Component Based Design</h3>
-                  <p class="text-sans-serif pb_font-16">Far far away, behind the word mountains, far from the countries
-                    Vokalia and Consonantia.</p>
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- END section -->
-
-
-  <section class="pb_section bg-light pb_slant-white" id="section-pricing">
+  <section class="pb_section bg-light pb_slant-white pb_pb-220" id="section-pricing">
     <div class="container">
       <div class="row justify-content-center mb-5">
         <div class="col-md-6 text-center mb-5">
@@ -367,9 +298,9 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
     </div>
   </section>
   <!-- ENDs ection -->
+   
   
-  
-  <section class="pb_section pb_slant-light pb_pb-220" id="section-reviews">
+  <section class="pb_section pb_slant-light " id="section-reviews">
     <div class="container">
       <div class="row justify-content-center mb-1">
         <div class="col-md-6 text-center mb-5">
@@ -386,7 +317,7 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
                 <div class="media d-block text-center testimonial_v1 pb_quote_v2">
                   <div class="media-body">
                     <div class="quote">&ldquo;</div>
-                    <blockquote class="mb-5">Заказывал работу на тему "Разработка информационной системы складского учета". 
+                    <blockquote class="mb-5">Заказывал разработать информационную систему учета клиентов для автосервиса. 
                     Стек: PHP, YII, MySQL. Сдал на отлично, спасибо!</blockquote>
                     <p>
                       <i class="ion-ios-star text-warning"></i>
@@ -491,8 +422,8 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
   </section>
   <!-- END section -->
   
-
-  <section class="pb_section pb_slant-white" id="section-faq">
+  
+  <section class="pb_section bg-light pb_slant " id="section-faq">
     <div class="container">
       <div class="row justify-content-center mb-5">
         <div class="col-md-6 text-center mb-5">
