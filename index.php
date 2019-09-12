e4bee3bf18be5beb309518ebedf75792b6c00c8f
olshec <olshec@gmail.com>
2019-09-12 04:04:36 -0400

    change text landing

--- index.php
+++ index.php
@@ -180,7 +180,7 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
           <div class="media d-block pb_feature-v1 text-left">
             <div class="pb_icon"><i class="ion-ios-paperplane-outline pb_icon-gradient"></i></div>
             <div class="media-body">
-              <h5 class="mt-0 mb-3 heading">Простой код</h5>
+              <h5 class="mt-0 mb-3 heading">Простой и понятный код</h5>
               <p class="text-sans-serif">В нашем коде разберется даже новичок</p>
             </div>
           </div>
@@ -335,8 +335,8 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
       <div class="row justify-content-center mb-1">
         <div class="col-md-6 text-center mb-5">
           <h5 class="text-uppercase pb_font-15 mb-2 pb_color-dark-opacity-3 pb_letter-spacing-2">
-            <strong>Reviews</strong></h5>
-          <h2>Good Review From Good People</h2>
+            <strong>Отзывы</strong></h5>
+          <h2>Хорошие отзывы от хороших людей</h2>
         </div>
       </div>
       <div class="row">
@@ -347,18 +347,17 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
                 <div class="media d-block text-center testimonial_v1 pb_quote_v2">
                   <div class="media-body">
                     <div class="quote">&ldquo;</div>
-                    <blockquote class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and
-                      Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast
-                      of the Semantics, a large language ocean.</blockquote>
+                    <blockquote class="mb-5">Заказывал работу на тему "Разработка информационной системы складского учета". 
+                    Стек: PHP, YII2, MariaDB. Сдал на отлично, спасибо!</blockquote>
                     <p>
                       <i class="ion-ios-star text-warning"></i>
                       <i class="ion-ios-star text-warning"></i>
                       <i class="ion-ios-star text-warning"></i>
+                      <i class="ion-ios-star text-warning"></i>
                       <i class="ion-ios-star-half text-warning"></i>
-                      <i class="ion-ios-star-outline text-warning"></i>
                     </p>
-                    <h3 class="heading">Garry Alexander</h3>
-                    <span class="subheading">@garry</span>
+                    <h3 class="heading">Борис Николаевич</h3>
+                    <span class="subheading">brsnikol@gmail.com</span>
 
                   </div>
                 </div>
@@ -458,38 +457,28 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
       <div class="row justify-content-center mb-5">
         <div class="col-md-6 text-center mb-5">
           <h5 class="text-uppercase pb_font-15 mb-2 pb_color-dark-opacity-3 pb_letter-spacing-2">
-            <strong>Pricing</strong></h5>
-          <h2>Choose Your Plans</h2>
+            <strong>Цены</strong></h5>
+          <h2>Выберите свой тип работы</h2>
         </div>
       </div>
       <div class="row">
         <div class="col-md">
           <div class="pb_pricing_v1 p-5 border text-center bg-white">
-            <h3>Basic</h3>
-            <span class="price"><sup>$</sup>19<span>month</span></span>
-            <p class="pb_font-15">Far far away, behind the word mountains, far from the countries Vokalia and
-              Consonantia, there live the blind texts</p>
-            <p class="mb-0"><a href="#" role="button" class="btn btn-secondary">Get started</a></p>
+            <h3>Бакалаврская</h3>
+            <span class="price"><sup>Р</sup>16 000</span>
+            <p class="pb_font-15">Бакалаврские работы от 16 000. Точную цену уточняйте.</p>
+            <p class="mb-0"><a href="#" role="button" class="btn btn-primary btn-shadow-blue">Заказать</a></p>
           </div>
         </div>
         <div class="col-md">
           <div class="pb_pricing_v1 p-5 border border-primary text-center bg-white">
-            <h3>Business</h3>
-            <span class="price"><sup>$</sup>39<span>month</span></span>
-            <p class="pb_font-15">Far far away, behind the word mountains, far from the countries Vokalia and
-              Consonantia, there live the blind texts</p>
-            <p class="mb-0"><a href="#" role="button" class="btn btn-primary btn-shadow-blue">Get started</a></p>
+            <h3>Магистерская диссертация</h3>
+            <span class="price"><sup>Р</sup>20 000</span>
+            <p class="pb_font-15">Магистерские диссертации от 20 000. Точную цену уточняйте.</p>
+            <p class="mb-0"><a href="#" role="button" class="btn btn-primary btn-shadow-blue">Заказать</a></p>
           </div>
         </div>
-        <div class="col-md">
-          <div class="pb_pricing_v1 p-5 border text-center bg-white">
-            <h3>Free</h3>
-            <span class="price"><sup>$</sup>99<span>month</span></span>
-            <p class="pb_font-15">Far far away, behind the word mountains, far from the countries Vokalia and
-              Consonantia, there live the blind texts</p>
-            <p class="mb-0"><a href="#" role="button" class="btn btn-secondary">Get started</a></p>
-          </div>
-        </div>
+
       </div>
     </div>
   </section>
@@ -499,9 +488,9 @@ $_SESSION['captcha_code'] = $captcha->getPhrase();
     <div class="container">
       <div class="row justify-content-center mb-5">
         <div class="col-md-6 text-center mb-5">
-          <h5 class="text-uppercase pb_font-15 mb-2 pb_color-dark-opacity-3 pb_letter-spacing-2"><strong>FAQ</strong>
+          <h5 class="text-uppercase pb_font-15 mb-2 pb_color-dark-opacity-3 pb_letter-spacing-2"><strong>ЧАВО</strong>
           </h5>
-          <h2>Frequently Ask Questions</h2>
+          <h2>Часто задаваемые вопросы</h2>
         </div>
       </div>
       <div class="row">
